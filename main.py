13010f40b24392213032b5775b46441e2e226599
Virender Kumar Jangra <jangra.viren@gmail.com>
2023-07-28 23:39:42 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+
+from google.cloud import aiplatform
+from langchain import LangChain
+
+
+def main():
+    # create a vertex ai client 
+    client = aiplatform.gapic.EngineServiceClient()
+
+    
+    #Create a model name
+    modelName = "todoModel"
+    
+    #create a model path to by joining the path
+    modelPath = os.path.join("models",modelName)
+    
+    #lets now create a langChain framework model
+    llm = LangChain()
+    
+    #now lets load the data in to this model
+    llm.load(modelPath)
+    
+    #lets now make a prediction 
+    prediction = llm.predict("What are my today's todos")
+    
+    print(prediction)
+    
+if __name__ == "__main__":
+    main()
